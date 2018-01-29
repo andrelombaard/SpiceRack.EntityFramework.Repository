3d7992608ea412cff0433c9f0ffd91dd5e37b2cf
Andre Lombaard <allombaard@gmail.com>
2018-01-29 18:55:18 +0200

    Implemented the ConfigurationManager

---
+++ SpiceRack.EntityFramework.Repository.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SpiceRack.EntityFramework.Repository.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SpiceRack.EntityFramework.Repository.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8f34ca09-b7d3-43ea-bdb4-5e82166f9454")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SpiceRack.EntityFrameworkCore.Repository/ConfigurationManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+
+namespace SpiceRack.EntityFrameworkCore.Repository
+{
+    public static class ConfigurationManager
+    {
+        private static Dictionary<string, string> ConnectionStrings = new Dictionary<string, string>();
+
+        public static string GetConnectionString(string name)
+        {
+            if (ConnectionStrings.Count < 1)
+            {
+                ReadConnectionStrings();
+            }
+
+            return ConnectionStrings[name];
+        }
+
+        private static void ReadConnectionStrings()
+        {
+            var codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            var uri = new UriBuilder(codeBase);
+            var path = Uri.UnescapeDataString(uri.Path);
+            var directory = Path.GetDirectoryName(path);
+
+            var files = Directory.GetFiles(directory, "*.config");
+
+            var xmlDocument = new XmlDocument();
+            xmlDocument.Load(files[0]);
+
+            var connectionStringNodes = xmlDocument.DocumentElement.SelectNodes("connectionStrings");
+
+            var connectionStrings = connectionStringNodes[0].ChildNodes;
+
+            foreach (XmlNode connectionString in connectionStrings)
+            {
+                ConnectionStrings.Add
+                    (
+                        connectionString.Attributes["name"].InnerText, 
+                        connectionString.Attributes["connectionString"].InnerText
+                    );
+            }
+        }
+    }
+}
